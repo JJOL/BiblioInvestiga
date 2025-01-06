8274196244d21786540b28bae218229a4fd3e4a4
JJOL <jjoulk@gmail.com>
2025-01-06 17:50:31 -0600

    clean desktop backend

--- compile.ps1
+++ compile.ps1
@@ -2,7 +2,7 @@ cd reader-app
 npm run build
 cd ..
 cp -Recurse -Force reader-app/dist desktop-app
-cp backend/backend-core.js desktop-app/backend-backend.js
+cp backend/backend-core.js desktop-app/backend-core.js
 cd desktop-app
 npm run start
 cd ..

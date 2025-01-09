a2aa7d644dcf9bf510f4215b94a80da96b7be405
jjol <jjoulk@gmail.com>
2025-01-09 14:52:13 -0600

    test new version/mac

--- desktop-app/forge.config.js
+++ desktop-app/forge.config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       name: '@electron-forge/maker-dmg',
       config: {
-        background: './assets/dmg-background.png',
-        format: 'ULFO'
+        // background: './assets/dmg-background.png',
+        // format: 'ULFO'
       }
     },
     {
@@ -30,13 +30,13 @@ module.exports = {
       name: '@electron-forge/maker-rpm',
       config: {},
     },
-    {
-      name: '@electron-forge/maker-wix',
-      config: {
-        language: 1033,
-        manufacturer: 'MIsPS'
-      }
-    }
+    // {
+    //   name: '@electron-forge/maker-wix',
+    //   config: {
+    //     language: 1033,
+    //     manufacturer: 'MIsPS'
+    //   }
+    // }
   ],
   plugins: [
     {
